1080fbd49a462d35f52da61cd68043740b3157af
Karga <nicolasdavidojeda@gmail.com>
2025-07-02 18:13:13 -0300

    agregado footer

--- E-Commerce/src/components/Footer.jsx
+++ E-Commerce/src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import "../style/index.css"
+import "../style/footer.css"
 function Footer() {
   return (
     <footer>
-      © 2025 Gaby's Gift
+      <h1>
+        © 2025 Gaby's Gift
+      </h1>
+      <img src="https://i.imgur.com/pxnct1B.png" alt="Gaby's Gifts" className="footer-logo" />
     </footer>
   );
 }
